3a5b9f134c815a733e19122bc08ec08477a4b6ea
2egn <103644239+2egn@users.noreply.github.com>
2022-09-30 15:18:53 +0900

    class M Killer

---
+++ classM_Killer.bat
@@ -0,0 +1,13 @@
+@echo off
+:loop
+set /a a+=1
+echo Now is %a% Looping....
+echo.
+echo ............................................................
+
+taskkill -F -T /PID hscagent.exe
+taskkill -F -T /PID hscfm.exe
+taskkill -F -T /PID hscrelay.exe
+taskkill -F -T /PID hscdm.exe
+cls
+goto loop
